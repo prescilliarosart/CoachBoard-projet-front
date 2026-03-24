1a3dc8bdcd21628917ce24556715d78415abe6ef
Kevan Gautier <kevangautier@gmail.com>
2026-03-24 15:16:49 +0100

    feat: ajout des types du context

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,2 +1,15 @@
 import { jwtDecode } from "jwt-decode";
 import { createContext, useState } from "react";
+
+export type User = {
+	id: number;
+	email: string;
+	role: "coach" | "eleve";
+};
+
+type AuthContextType = {
+	user: User | null;
+	token: null | string;
+	login: (token: string) => void;
+	logout: () => void;
+};
