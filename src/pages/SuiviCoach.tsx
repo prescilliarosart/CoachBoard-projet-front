2ef9cea7a7d9d0e74b38b80310b451d9558abb06
Kevan Gautier <kevangautier@gmail.com>
2026-04-09 14:38:47 +0200

    feat: ajout des données fictives

--- src/pages/SuiviCoach.tsx
+++ src/pages/SuiviCoach.tsx
@@ -41,3 +41,53 @@ const navLinks = [
 	{ label: "Élèves", path: "/eleves" },
 	{ label: "Suivi", path: "/suivi" },
 ];
+
+const exercicesDisponibles = ["Squat", "Développé couché", "Soulevé de terre"];
+
+const progressionData: Record<string, { semaine: string; poids: number }[]> = {
+	Squat: [
+		{ semaine: "S1", poids: 75 },
+		{ semaine: "S2", poids: 78 },
+		{ semaine: "S3", poids: 80 },
+		{ semaine: "S4", poids: 83 },
+		{ semaine: "S5", poids: 90 },
+		{ semaine: "S6", poids: 87 },
+		{ semaine: "S7", poids: 95 },
+	],
+	"Développé couché": [
+		{ semaine: "S1", poids: 60 },
+		{ semaine: "S2", poids: 62 },
+		{ semaine: "S3", poids: 65 },
+		{ semaine: "S4", poids: 65 },
+		{ semaine: "S5", poids: 67 },
+		{ semaine: "S6", poids: 70 },
+		{ semaine: "S7", poids: 72 },
+	],
+	"Soulevé de terre": [
+		{ semaine: "S1", poids: 100 },
+		{ semaine: "S2", poids: 105 },
+		{ semaine: "S3", poids: 107 },
+		{ semaine: "S4", poids: 110 },
+		{ semaine: "S5", poids: 115 },
+		{ semaine: "S6", poids: 112 },
+		{ semaine: "S7", poids: 120 },
+	],
+};
+
+const performancesData = [
+	{ id: 1, poids: "95 KG", exercice: "Squat", repetition: 5, effort: "4/5" },
+	{
+		id: 2,
+		poids: "72 KG",
+		exercice: "Développé couché",
+		repetition: 8,
+		effort: "3/5",
+	},
+	{
+		id: 3,
+		poids: "120 KG",
+		exercice: "Soulevé de terre",
+		repetition: 3,
+		effort: "5/5",
+	},
+];
