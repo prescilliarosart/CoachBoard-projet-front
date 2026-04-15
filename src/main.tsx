5d24fe32701010abb1c4149434a5d6baa94405f4
Kevan Gautier <kevangautier@gmail.com>
2026-04-15 08:51:36 +0200

    feat: ajout d'un createTheme dans le main pour uniformiser

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,29 @@
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import { AuthProvider } from "./context/AuthContext.tsx";
 
+const theme = createTheme({
+	palette: {
+		warning: {
+			main: "#22c55e",
+			contrastText: "#0b1520",
+		},
+	},
+});
+
 const root = document.getElementById("root");
 if (root) {
 	ReactDOM.createRoot(root).render(
 		<React.StrictMode>
 			<BrowserRouter>
-				<AuthProvider>
-					<App />
-				</AuthProvider>
+				<ThemeProvider theme={theme}>
+					<AuthProvider>
+						<App />
+					</AuthProvider>
+				</ThemeProvider>
 			</BrowserRouter>
 		</React.StrictMode>,
 	);
